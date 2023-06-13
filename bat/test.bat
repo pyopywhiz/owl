d30e7764e66f45280aafcd5a475268255da54ef9
pyopywhiz <pyopywhiz@email.com>
2023-06-14 00:51:17 +0700

    Delete test.bat

--- bat/test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set chrome_cookies_path=C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Profile 1\Network\Cookies
-set output_file=cookies_decrypted.txt
-echo Decrypting Chrome cookies...
-"ChromeCookiesView.exe" /decrypt_cookies "%chrome_cookies_path%" /scomma "%output_file%"
-echo Done.
-pause
