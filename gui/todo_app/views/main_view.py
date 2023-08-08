88c01dbeb00b9037e66c5bffa8d2cffcd68b2cf8
pyopywhiz <acc0568074190@gmail.com>
2023-08-09 00:28:13 +0700

    Add main view

--- gui/todo_app/views/main_view.py
+++ gui/todo_app/views/main_view.py
@@ -1,29 +1,10 @@
-from typing import Any
-
-from PyQt5.QtWidgets import QMainWindow, QDesktopWidget, QWidget, QVBoxLayout
-
 from gui.todo_app.views.todo_view import TodoView
 
 
-class MainView(QMainWindow):
-    def __init__(self) -> None:
-        super().__init__()
+class MainView:
+    def __init__(self, controller):
+        self.controller = controller
+        self.todo_view = TodoView(controller)
 
-        self.setWindowTitle("Todo List App")
-        self.resize(600, 600)
-
-        qr_value = self.frameGeometry()
-        cp_value = QDesktopWidget().availableGeometry().center()
-        qr_value.moveCenter(cp_value)
-        self.move(qr_value.topLeft())
-
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-
-        self.layout: Any = QVBoxLayout()
-        self.central_widget.setLayout(self.layout)
-
-        todo_view = TodoView()
-
-        self.layout.addWidget(todo_view)
-        self.setLayout(self.layout)
+    def show(self):
+        pass
