0e98b41d2cedbbe35b9dc82d788ac1ff1b16c607
pyopywhiz <acc0568074190@gmail.com>
2023-08-10 01:04:53 +0700

    Think a lot

--- gui/todo_app/controllers/main_controller.py
+++ gui/todo_app/controllers/main_controller.py
@@ -3,7 +3,8 @@ from gui.todo_app.views.main_view import MainView
 
 class MainController:
     def __init__(self):
-        self.view = MainView(self)
+        self.todo_controller
+        self.view = MainView()
 
     def show(self):
         self.view.show()
--- gui/todo_app/views/main_view.py
+++ gui/todo_app/views/main_view.py
@@ -1,11 +1,25 @@
+from gui.todo_app.views.todo_list_view import TodoListView
 from gui.todo_app.views.todo_view import TodoView
 
 
 class MainView:
-    def __init__(self, controller):
-        self.controller = controller
-        self.todo_view = TodoView(controller)
+    def __init__(self, controllers ):
+        self.setWindowTitle("Todo List App")
+        self.resize(600, 600)
+
+        qr_value = self.frameGeometry()
+        cp_value = QDesktopWidget().availableGeometry().center()
+        qr_value.moveCenter(cp_value)
+        self.move(qr_value.topLeft())
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        self.layout: Any = QVBoxLayout()
+        self.central_widget.setLayout(self.layout)
+
+        self.todo_view = TodoView()
+        self.todo_list_view = TodoListView()
 
     def show(self):
         self.todo_view.show()
-
